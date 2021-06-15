b7e9a76844a32338424ed4d42743029463a58f70
pradeepmis91 <68383529+pradeepmis91@users.noreply.github.com>
2021-06-15 14:44:40 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -2,6 +2,18 @@ variable "org_name" {}
 variable "api_token" {}
 variable "base_url" {}
 
+#provider information
+terraform {
+    required_providers {
+        okta = {
+            source = "okta/okta"
+            version = "~>3.10"
+        }
+    }
+}
+
+#configure the okta provider
+
 provider "okta" {
     org_name = var.org_name
     base_url = var.base_url
