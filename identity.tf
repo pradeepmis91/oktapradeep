854bfab73df84dd761dd0fafde6a606f17adf9c5
pradeepmis91 <68383529+pradeepmis91@users.noreply.github.com>
2021-06-18 20:38:58 +0530

    Update identity.tf

--- identity.tf
+++ identity.tf
@@ -2,15 +2,6 @@ variable "org_name" {}
 variable "api_token" {}
 variable "base_url" {}
 
-
-#configure the okta provider
-
-provider "okta" {
-    org_name = var.org_name
-    base_url = var.base_url
-    api_token = var.api_token
-}
-
 resource "oktapradeep" "dob_extension" {
   index  = "date_of_birth"
   title  = "Date of Birth"
