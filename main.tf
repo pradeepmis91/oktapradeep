c76c416b10f50420e8206d8d5b5474f3b71b3a18
pradeepmis91 <68383529+pradeepmis91@users.noreply.github.com>
2021-06-18 20:34:36 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    okta = {
-      source  = "my-host/me/okta"                          
-      version = "4.0.0"                                               <- version can be whatever you like
-    }
-  }
-  required_version = ">= 0.13"
-}
