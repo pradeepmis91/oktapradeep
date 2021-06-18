e31ef82fb62b22df9ebab7eef4f3d5fa19da3e48
pradeepmis91 <68383529+pradeepmis91@users.noreply.github.com>
2021-06-18 21:11:32 +0530

    Update Identity.tf

--- Identity.tf
+++ Identity.tf
@@ -1,11 +1,23 @@
+# https://www.terraform.io/docs/configuration/variables.html
 variable "org_name" {}
 variable "api_token" {}
 variable "base_url" {}
 
+terraform {
+  required_providers {
+    okta = {
+      version = "~> 3.5.0"
+      source  = "oktadeveloper/okta"
+    }
+  }
+  required_version = ">= 0.13"
+}
+
+# More https://www.terraform.io/docs/configuration/providers.html and https://www.terraform.io/docs/providers/okta/index.html
 provider "okta" {
-    org_name = var.org_name
-    base_url = var.base_url
-    api_token = var.api_token
+  org_name  = var.org_name
+  api_token = var.api_token
+  base_url  = var.base_url
 }
 
 resource "okta_user_schema" "dob_extension" {
